03eede7272cdc46b8f8c8aad5ba12096d25a3c19
Gamer492GG <mal527283@gmail.com>
2026-07-03 23:33:30 +0500

    Mega, yber,piska Game 2.0 Добавлен while Изменён выбор ответа Изменён дизайн заголовка

--- src/Main.java
+++ src/Main.java
@@ -3,80 +3,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int score = 0;
+        String Tool;
+        int block = 1;
         int max = 3;
         int min = 1;
-        int randomBlock = (int) (Math.random() * (max - min + 1)) + min;
+        // int randomBlock = (int) (Math.random() * (max - min + 1)) + min;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Игра \"Сломай Блок\"");
+        System.out.println("                    Игра \"Сломай Блок\"");
         System.out.println("Что бы сломать блок перед тобой используй инструмент на выбор:");
-        System.out.println("Кирка|Лопата|Меч");
-        System.out.println("Готов?");
-        System.out.println("Да/Нет");
+        System.out.println("                    Кирка|Лопата|Меч");
+        System.out.println("                          Начать?");
+        System.out.println("                          Да/Нет");
         String Start = scanner.next();
-        if (Start.equals("Да")) System.out.println("Первый Блок");
-        else {System.out.println("А мне Похуй))");
-            System.out.println("Первый Блок)");}
-    switch (randomBlock) {
-        case 1 -> {
-            System.out.println("Перед тобой Глина");
-            String Tool = scanner.next();
-            switch (Tool) {
-                case "Лопата", "лопата", "ЛОПАТА" -> score += 1;
-                default -> System.out.println("Анлак");}}
-        case 2 -> {
-            System.out.println("Перед тобой Камень");
-            String Tool = scanner.next();
-            switch (Tool) {
-                case "Кирка", "кирка", "КИРКА" -> score += 1;
-                default -> System.out.println("Анлак");}}
-        case 3 -> {
-            System.out.println("Перед тобой Паутина");
-            String Tool = scanner.next();
-            switch (Tool) {
-                case "Меч", "меч", "МЕЧ" -> score += 1;
-                default -> System.out.println("Анлак");}}}
-        System.out.println("Второй Блок");
-        randomBlock = (int) (Math.random() * (max - min + 1)) + min;
-    switch (randomBlock){
-            case 1 -> {
-                System.out.println("Перед тобой Глина");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Лопата", "лопата", "ЛОПАТА" -> score += 1;
-                    default -> System.out.println("Анлак");}}
-            case 2 -> {
-                System.out.println("Перед тобой Камень");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Кирка", "кирка", "КИРКА" -> score += 1;
-                    default -> System.out.println("Анлак");}}
-            case 3 -> {
-                System.out.println("Перед тобой Паутина");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Меч", "меч", "МЕЧ" -> score += 1;
-                    default -> System.out.println("Анлак");}}}
-        System.out.println("Третий Блок");
-        randomBlock = (int) (Math.random() * (max - min + 1)) + min;
-    switch (randomBlock){
-            case 1 -> {
-                System.out.println("Перед тобой Глина");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Лопата", "лопата", "ЛОПАТА" -> score += 1;
-                    default -> System.out.println("Анлак");}}
-            case 2 -> {
-                System.out.println("Перед тобой Камень");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Кирка", "кирка", "КИРКА" -> score += 1;
-                    default -> System.out.println("Анлак");}}
-            case 3 -> {
-                System.out.println("Перед тобой Паутина");
-                String Tool = scanner.next();
-                switch (Tool) {
-                    case "Меч", "меч", "МЕЧ" -> score += 1;
-                    default -> System.out.println("Анлак");}}}
-        System.out.println("Ты набрал " + score + " Очков");
+        switch (Start) {
+            case "Да" : while (block < 4 ) {
+                System.out.println("Перед тобой "+block+" Блок");
+                block++;
+                int randomBlock = (int) (Math.random() * (max - min + 1)) + min;
+                switch (randomBlock) {
+                    case 1 -> {
+                        System.out.println("Перед тобой Глина");
+                        Tool = scanner.next().toLowerCase();
+                        if (Tool.equals("лопата")) {
+                            score++;} else {
+                            System.out.println("Анлак");}}
+                    case 2 -> {
+                        System.out.println("Перед тобой Камень");
+                        Tool = scanner.next().toLowerCase();
+                        if (Tool.equals("кирка")) {
+                            score++;} else {
+                            System.out.println("Анлак");}}
+                    case 3 -> {
+                        System.out.println("Перед тобой Паутина");
+                        Tool = scanner.next().toLowerCase();
+                        if (Tool.equals("меч")) {
+                            score++;} else {
+                            System.out.println("Анлак");}}}}
+                System.out.println("Ты набрал " + score + " Очков");
+            case "Нет" : break;
+            default:{
+                System.out.println("Ошибка, программа закрывается.");
+                break;}
+        }
     }
 }
